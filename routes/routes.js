bbcf9eb17705d50a73b8eb0d5d806262fb38294d
Matt Wilkin <mwilkin22@gmail.com>
2019-06-09 22:00:05 -0700

    add test files, db directory, update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,21 +3,33 @@
 const express = require('express');
 const appRouter = express.Router();
 const db = require('./pg.js');
+// let db;
+
+// const args = process.argv.slice(2);
+
+// if(args[0] === 'mongo'){
+//   db = require('../data/mongo/mongo.js');
+//   console.log('running mongo');
+
+// } else {
+//   db = require('./pg.js');
+//   console.log('running in pg');
+
+// }
 
 
 // API Routes
-appRouter.get('/', getBooks);
-appRouter.post('/searches', createSearch);
-appRouter.get('/searches/new', newSearch);
-appRouter.get('/books/:id', getBook);
-appRouter.post('/books', createBook);
-appRouter.put('/books/:id', updateBook);
-appRouter.delete('/books/:id', deleteBook);
+appRouter.get('/', db.getBooks);
+appRouter.post('/searches', db.createSearch);
+appRouter.get('/searches/new', db.newSearch);
+appRouter.get('/books/:id', db.getBook);
+appRouter.post('/books', db.createBook);
+appRouter.put('/books/:id', db.updateBook);
+appRouter.delete('/books/:id', db.deleteBook);
 
 appRouter.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
 module.exports = appRouter;
-
 
 
 const pg = require('pg');
